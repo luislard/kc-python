f71f2a6ef90716efaa1a64129604d6a6eba20645
Luis Rosales <eng.luisrosales@gmail.com>
2017-12-05 00:34:14 +0100

    creating the db with migrations

--- src/movies/models.py
+++ src/movies/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
+
+class Movie(models.Model):
+
+    title = models.CharField(max_length=150)
+    summary = models.TextField()
+    director_name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    image = models.URLField()
+    rating = models.FloatField()
+
+    created_at = models.DateTimeField(auto_now_add=True) # set date when object is created
+    modified_at = models.DateTimeField(auto_now=True)  # saves the date when the object is updated
