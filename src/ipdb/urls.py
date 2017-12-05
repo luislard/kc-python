3403bf2969454a1dec82c58a54601e2f20a19fdf
Luis Rosales <eng.luisrosales@gmail.com>
2017-12-05 23:42:31 +0100

    creamos una pagina de 404 y creamos una pagina de detalle de las pelis

--- src/ipdb/urls.py
+++ src/ipdb/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from movies.views import hello_world, home
+from movies.views import hello_world, home, movie_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', hello_world)
-    path('', home)
+    path('', home),
+    path('movies/<int:pk>', movie_detail)
 ]
 
--- src/movies/views.py
+++ src/movies/views.py
@@ -14,6 +14,17 @@ def hello_world(request):
 
 def home(request):
     latest_movies = Movie.objects.all().order_by('-release_date')
-    html = "<ul>"
     context = { 'movies': latest_movies[:3] }
     return render(request, "home.html", context)
+
+
+def movie_detail(request, pk):
+    possible_movies = Movie.objects.filter(pk=pk)
+    if len(possible_movies) == 0:
+        # pelicula no existe
+        return render(request,"404.html", status=404)
+    else:
+        # pelicula existe
+        movie = possible_movies[0]
+        context = { 'movie': movie }
+        return render(request, "movie_detail.html", context)
