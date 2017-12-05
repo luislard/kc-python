4e4bbb349081f1c4ca527b57af23694c4bf01430
Luis Rosales <eng.luisrosales@gmail.com>
2017-12-05 22:22:37 +0100

    listamos las peliculas

--- src/movies/views.py
+++ src/movies/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 
+
+from movies.models import Movie
+
 # definimos una funcion
 def hello_world(request):
     name = request.GET.get("name")
@@ -7,3 +10,12 @@ def hello_world(request):
         return HttpResponse("Hello World!")
     else:
         return HttpResponse("Hello " + name)
+
+
+def home(request):
+    movies = Movie.objects.all()
+    html = "<ul>"
+    for movie in movies:
+        html += "<li>" + movie.title + "</li>"
+    html += "</ul>"
+    return HttpResponse(html)
