de737ae32c14816492c32b9a8593b88f05ef2ca2
Luis Rosales <eng.luisrosales@gmail.com>
2017-12-07 22:20:17 +0100

    adding user login and logout

--- src/ipdb/urls.py
+++ src/ipdb/urls.py
@@ -17,11 +17,15 @@ from django.contrib import admin
 from django.urls import path
 
 from movies.views import hello_world, home, movie_detail
+from users.views import login, logout
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', hello_world)
     path('', home, name="home_page"),
-    path('movies/<int:pk>', movie_detail, name="movie_detail_page")
+    path('movies/<int:pk>', movie_detail, name="movie_detail_page"),
+    path('login', login, name="login_page"),
+    path('logout', logout, name="logout_page"),
+
 ]
 
---
+++ src/users/views.py
@@ -0,0 +1,22 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login as django_login, logout as django_logout
+from django.shortcuts import render, redirect
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('login_username')
+        password = request.POST.get('login_password')
+        authenticated_user = authenticate(username=username,password=password)
+        if authenticated_user and authenticated_user.is_active:
+            django_login(request, authenticated_user)
+            return redirect('home_page')
+        else:
+            messages.error(request, 'usuario incorrecto o inactivo')
+            print('usuario incorrecto')
+
+    return render(request, 'login_form.html')
+
+def logout(request):
+    django_logout(request)
+    return redirect('login_page')
