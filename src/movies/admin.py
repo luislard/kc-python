b8df7f43781b6b54b5e7ae943429cd1a65aa275a
Luis Rosales <eng.luisrosales@gmail.com>
2017-12-11 23:36:38 +0100

    agregamos el buscar por nombre de categoria

--- src/movies/admin.py
+++ src/movies/admin.py
@@ -13,8 +13,9 @@ class MovieAdmin(admin.ModelAdmin):
 
     list_display = ('title', 'release_date', 'rating', 'user_full_name', 'category') # muestra estas columnas en el listado de pelis
     list_filter = ('user', 'category', 'release_date') # agrega los paneles de filtrado a la derecha
-    search_fields = ('title', 'director_name', 'summary') # agrega la casilla de buscar y busca por estas columnas
+    search_fields = ('title', 'director_name', 'summary', 'category__name') # agrega la casilla de buscar y busca por estas columnas
 
     def user_full_name(self, obj):
         return "{0} {1}".format(obj.user.first_name, obj.user.last_name)
     user_full_name.short_description = "Movie Owner" # en python todo son objetos
+    user_full_name.admin_order_field = "user__first_name" # le decimos que ordene por el first name
