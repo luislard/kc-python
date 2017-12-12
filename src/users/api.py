25bc35daa49e9cfed070d5e0735bc5c7fd71ad69
Luis Rosales <eng.luisrosales@gmail.com>
2017-12-13 00:05:24 +0100

    ahora se puede actualizar controlando que al actualizar no se pueda repetir el campo username

--- src/users/api.py
+++ src/users/api.py
@@ -42,3 +42,12 @@ class UserDetailAPI(APIView):
         user = get_object_or_404(User, pk=pk)
         serializer = UserSerializer(user)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        user = get_object_or_404(User, pk=pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- src/users/serializers.py
+++ src/users/serializers.py
@@ -18,12 +18,18 @@ class UserSerializer(UserListSerializer):
     password = serializers.CharField()
 
     def validate_username(self, data):
-        if User.objects.filter(username=data).exists():
-            raise ValidationError('Usuario already exists')
+        if self.instance is None and User.objects.filter(username=data).exists():
+            raise ValidationError('User already exists')
+        if self.instance and self.instance.username != data and User.objects.filter(username=data).exists():
+            raise ValidationError('Wanted username is already in use')
         return data
 
     def create(self, validated_data):
         instance = User()
+        return self.update(instance, validated_data)
+
+
+    def update(self, instance, validated_data):
         instance.first_name = validated_data.get("first_name")
         instance.last_name = validated_data.get("last_name")
         instance.username = validated_data.get("username")
@@ -31,8 +37,4 @@ class UserSerializer(UserListSerializer):
         instance.set_password(validated_data.get("password"))
         instance.save()
         return instance
-
-
-    def update(self, validated_data):
-        pass
 
